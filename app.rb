3e38e5e234af321b481c516604e3bbc0d8f5c7cc
jyoung-gds <jonathan.young@digital.cabinet-office.gov.uk>
2022-01-10 15:49:44 +0000

    First commit

---
+++ app.rb
@@ -0,0 +1,27 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+
+get '/' do
+  'Hello world!'
+end
+
+get '/secret' do
+  'psshwshwshpsh'
+end
+
+get '/voila' do
+  'aijsdjpcsaj'
+end
+
+get '/insult' do
+  'hehe ez'
+end
+
+get '/dog' do
+  %(
+  <div>
+    <img src="https://www.pixelstalk.net/wp-content/uploads/2016/08/Cute-Puppy-Background-Download-Free-1-620x388.jpg" style="border: 8px dashed red">
+  </div>
+  )
+
+end
